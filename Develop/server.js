4337619c2b6854fe484b3814f929d44351dadf3b
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-03 00:27:07 -0800

    both the landing page and notes.html work as expected

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const path = require('path');
+const fs = require('fs');
 const db = require('./db/db.json');
 const port = process.env.PORT || 3001;
 
@@ -10,8 +12,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 // GET route for retrieving all notes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 // GET route for Landing page
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
@@ -31,15 +36,15 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/i
 
 
 
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-
-});
 
